6c137d7010bcde3ba0ddd9e9afac2220a1eb1e5c
Michael Khokhar <kamran.michael@gmail.com>
2019-03-22 11:13:54 -0400

    code with comments

--- Exercise2.R
+++ Exercise2.R
@@ -1,21 +1,41 @@
-install.packages("dplyr")
+
+
+# Function to check whether package is installed
+is.installed <- function(mypkg){
+  is.element(mypkg, installed.packages()[,1])
+} 
+
+# check if package "dplyr" is installed
+if (!is.installed("dplyr")){
+  install.packages("dplyr")
+}
+
+
+# check if package "stringr" is installed
+if (!is.installed("stringr")){
+  install.packages("stringr")
+}
+
+
 library("dplyr")
-install.packages("stringr")
 library("stringr")
 
+
+#Load data into data frame, initialize all empty values to NA
 df_titanic <- read.csv("C:/Users/mkk/Documents/SpringBoard/Exercise/titanic/titanic_original.csv",na.strings = c("",NA))
 
-
+#Update empty embarked values with S
 df_titanic[is.na(df_titanic$embarked),11] <-"S"
 
+#update empty age values with mean
 df_titanic[is.na(df_titanic$age),5] <- mean(df_titanic[!is.na(df_titanic$age),5])
 
 temp_df_titanic<- df_titanic%>% mutate(has_cabin=1) #Adding column to store if cabin has number or not with default of 1 i.e every cabin has been number
 temp_df_titanic[is.na(temp_df_titanic$cabin),"has_cabin"] <- 0 #update has_cabin for the cabin which has NA value
 
-print(df_titanic[is.na(df_titanic$age),5])
-View(df_titanic)
+
+
 View(temp_df_titanic)
-print(mean(df_titanic$age))      
+
 
 
